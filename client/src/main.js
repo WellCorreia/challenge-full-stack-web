32a9883b4d7023813462d68c53c135da7d7921db
Wellington Correia <wellington.dossantos@montreal.com.br>
2020-11-08 16:10:14 -0300

    Adiciona o yarn.lock

--- client/src/main.js
+++ client/src/main.js
@@ -1,15 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
-new Vue({
+window._Vue = new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount('#app')
