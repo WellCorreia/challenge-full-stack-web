b0588824842a938acdeb12b630e4854ab4ccea82
Wellington Correia <wellington.dossantos@montreal.com.br>
2020-11-08 15:58:06 -0300

    Remove páginas padrões que foram criadas ao iniciar o projeto. Configura o arquivo App.vue.

---
+++ client/src/components/base/SideBar.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app>
+      <v-sheet
+        color="grey lighten-4"
+        class="pa-4"
+      >
+        <v-avatar
+          class="mb-4"
+          color="grey darken-1"
+          size="64"
+        ></v-avatar>
+
+        <div>john@vuetifyjs.com</div>
+      </v-sheet>
+
+      <v-divider></v-divider>
+      <v-list>
+        <v-list-item v-for="item in links" :key="item.icon" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <router-view/>
+
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    cards: ['Today', 'Yesterday'],
+    drawer: null,
+    links: [
+      {
+        icon: 'mdi-account',
+        title: 'Aluno',
+        to: '/student'
+      }
+    ]
+  })
+}
+</script>
