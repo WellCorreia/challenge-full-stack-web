dfb237a4be960a4348237f8ea6010ded18841542
Wellington Correia <wellington.dossantos@montreal.com.br>
2020-11-10 07:40:39 -0300

    Adiciona ajuste no jest.config e babel.config para conhecimentos de rotas.

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,3 +1,34 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest'
+  moduleFileExtensions: [
+    'js',
+    'jsx',
+    'json',
+    'vue',
+  ],
+  transform: {
+    '^.+\\.vue$': 'vue-jest',
+    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+    '^.+\\.jsx?$': 'babel-jest',
+  },
+  modulePaths: [
+    "<rootDir>/src",
+    "<rootDir>/node_modules"
+  ],
+  snapshotSerializers: [
+    'jest-serializer-vue',
+  ],
+  testMatch: [
+    '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)',
+  ],
+  transformIgnorePatterns: [
+    "<rootDir>/node_modules/(?!(vuetify)/)",
+  ],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+    'vuetify/lib(.*)': '<rootDir>/node_modules/vuetify/es5$1',
+  },
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
 }
